5dc4498ddb6ce7db084ed80b0baf31667d4b1f93
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-01-12 21:31:33 -0500

    Plus One

---
+++ answers/PlusOne.js
@@ -0,0 +1,9 @@
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+var plusOne = function(digits) {
+    let num = BigInt(digits.join('')) + BigInt(1)
+    return num.toString().split('').map(Number)
+};
+plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3])
