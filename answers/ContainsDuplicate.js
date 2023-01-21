c63405208c7c3239af73f40f67a027fbf8ce3534
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-01-20 22:14:20 -0500

    Contains Duplicate

---
+++ answers/ContainsDuplicate.js
@@ -0,0 +1,16 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+var containsDuplicate = function(nums) {
+    nums = nums.sort()
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === nums[i+1]){
+            return true
+        }
+    }
+    return false
+};
+
+
+
+containsDuplicate([1,2,3,4])
